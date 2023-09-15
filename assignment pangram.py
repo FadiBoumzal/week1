9c9c2d6ce42d179bd5358e8c58ba72c41d95cbb1
FadiBoumzal <144443553+FadiBoumzal@users.noreply.github.com>
2023-09-15 12:31:54 +0100

    Add files via upload

---
+++ assignment pangram.py
@@ -0,0 +1,15 @@
+pangram="The quick brown fox jumps over the lazy dog"
+print(pangram[1:5])
+print(pangram[17:19])
+print(pangram[:19])
+print(pangram[26:])
+
+Word1="Leaving"
+Word2="Certificate"
+Word3="Computer"
+Word4="Science"
+subjectname=Word1+ Word2+ Word3+ Word4
+print(subjectname)
+print(pangram[ :3] + pangram[16:])
+noun=(input("Enter a noun:"))
+print("The plural of ", noun, "is",noun+"s")
