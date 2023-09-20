f92e8ffcbf88592650fd74f0f87652aae2a64171
FadiBoumzal <144443553+FadiBoumzal@users.noreply.github.com>
2023-09-20 12:09:17 +0100

    Add files via upload

---
+++ lists assignment.py
@@ -0,0 +1,28 @@
+My_list1 = ["computer", "science"]
+My_list2 = ["computer","science",12,15.4]
+My_list3 = ["computer",'science',12,15.4,["inner","list"] ]
+print(My_list1)
+print(My_list2)
+for a in "alphabet":
+ print(a)
+My_list = ["Monday", "Tuesday", "Wednesday"]
+for y in My_list:
+ print(y)
+ 
+ b= ["Hello","world","I","need",'some','other','words',21,35.789]
+First_item = b[0]
+Third_item = b[2]
+Num_items_in_lst = len(b)
+Last_item = b[Num_items_in_lst -1]
+Last_item = b[-1]
+print(Last_item)
+
+thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
+print(thislist)
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
+thislist4 = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist4[:5])
+b = "Hello, World!"
+print(b[2:10:2]) 
