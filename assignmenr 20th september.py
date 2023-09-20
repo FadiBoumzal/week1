f65b8261f70e8d38dbf4a737d3fd9009cdb5e88a
FadiBoumzal <144443553+FadiBoumzal@users.noreply.github.com>
2023-09-20 13:12:25 +0100

    Add files via upload

---
+++ assignmenr 20th september.py
@@ -0,0 +1,11 @@
+my_list=["football, basketball"]
+print(my_list)
+sport=input("Enter a sport you like")
+print(sport)
+
+my_list2=["english", "maths", "science", "geography", "history", "irish"]
+print(my_list2)
+subject=input("enter a subject you dont like")
+
+my_list2.remove ( subject)
+print(my_list2)
