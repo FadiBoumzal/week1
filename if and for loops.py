33bf7d8a6b04aa6e48b046ebd30e74fb53db5123
FadiBoumzal <144443553+FadiBoumzal@users.noreply.github.com>
2023-09-29 12:31:43 +0100

    Add files via upload

---
+++ if and for loops.py
@@ -0,0 +1,30 @@
+number1=input("enter a number under 20")
+message=("thank you")
+number2=int(number1)
+if number2 >20:
+    print("number is too high")
+else:
+    print("thank you")
+    
+    
+number3=input("enter a number between 10-20")
+message1=("thank you")
+number4=int(number3)
+if number4<=20 and number4>=10:
+    print("thank you")
+else:
+    print("out of range")
+    
+    
+colour=input("enter favourite colour")
+str(colour)
+if colour.lower() == "red":
+    print("me too")
+else:
+        print("i prefer red")
+        
+
+#unfinished
+rain=("enter if its raining")
+rain2=rain.lower()
+if rain2=yes 
