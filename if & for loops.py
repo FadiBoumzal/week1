12419c95c8d21c2f4121c075e551ac5bb77b7fa6
FadiBoumzal <144443553+FadiBoumzal@users.noreply.github.com>
2023-10-06 12:29:55 +0100

    Add files via upload

---
+++ if & for loops.py
@@ -0,0 +1,31 @@
+#35    
+name=input("enter your name")
+
+for name1 in range(0,3,1):
+    print(name)
+    
+#36
+number3=input("enter a number")
+number4=int(number3)
+
+name=input("enter your name")
+for name1 in range(0,number4,1):
+    print(name)
+
+#37
+name2=input("enter your name")
+for name3 in name2:
+    print(name3)
+
+#38
+name2=input("enter your name")
+
+number5=input("enter a number")
+number5=int(number5)
+for name3 in range(0,number5,1):
+
+ for name3 in name2:
+    print(name3)
+
+    
+
