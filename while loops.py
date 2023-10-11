e096376101fd042edfa31ac2015bc54282315ac4
FadiBoumzal <144443553+FadiBoumzal@users.noreply.github.com>
2023-10-11 13:14:34 +0100

    Add files via upload

---
+++ while loops.py
@@ -0,0 +1,13 @@
+total1 = 0
+total1 = int(total1)
+total3=0
+while total1 <= 50:
+    total2=input("enter a number ")
+    total2 = int(total2)
+    total1 = total1 + total2
+    
+   
+    total3=int(total2)
+    total3=total3+total2
+    print(total3)
+    
