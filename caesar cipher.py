8a3145cedb98235518c67debca79b1aaff649871
FadiBoumzal <144443553+FadiBoumzal@users.noreply.github.com>
2023-10-20 12:33:17 +0100

    Add files via upload

---
+++ caesar cipher.py
@@ -0,0 +1,13 @@
+# d e f g h i j k l m n o p q r s t u v w x y z a b c d
+
+# DWKORQH FRPPXQWB FROOJH
+# ATHLONE COMMUNTY COLLGE
+
+# 25 possible keys
+
+# What do you get when you cross a snowman with a vanpire? Frostbite
+
+caesar_cipher = input("enter your message")
+key= input("enter a number")
+key = int(key)
+coding_option = input("Encoded or Decoded")
